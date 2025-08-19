c3cbcee43b13e9d9ea83afdba20ea77108f5cd6d
mahmoud tarek <mahmoudelgamily328@gmail.com>
2025-08-19 20:10:16 +0300

    2

--- src/app.controller.js
+++ src/app.controller.js
@@ -9,9 +9,9 @@ const Bootstrap=(app,express)=>{
 
 app.use(express.json())
 Connection()
-app.get("/",(req,res,next)=>{
-  res.send(json({message:"Welcome to my library system"}))
-})
+  app.get("/", (req, res) => {
+  res.send("Welcome to app..............✌️💙");
+});
 app.use("/api/user",userRouter)
 app.use("/api/books",bookRouter)
 app.use("/api/transaction",transactionRouter)
